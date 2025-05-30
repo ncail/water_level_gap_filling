0ea412e94649665d3af798acdaa6999fa0482cff
ncail <ncail@islander.tamucc.edu>
2025-05-29 19:55:24 -0500

    added wind values, can be of varying frequency, eg 6-min or hourly.

--- generate_mlp_inputs.py
+++ generate_mlp_inputs.py
@@ -1,5 +1,9 @@
 """
 Assumes columns of data are in the order: datetime, water level, wind speed, wind direction.
+Config window sizes are given in units [number of water level points, number of wind points].
+Gets water levels from paired station, winds at target station, and target water levels at target station.
+Config allows option to request converting 6-minute wind to hourly-averaged wind.
+Wind speed and wind direction are converted to UV wind vectors.
 """
 
 # Imports.
@@ -12,31 +16,51 @@ import numpy as np
 import helpers
 
 
+''' *************************************************** GET DATA *************************************************** '''
 # Get paths to preprocessed data from config_mlp_input_generation.json.
 config = helpers.load_configs("config_mlp_input_generation.json")
 file_paths = config['file_paths']
 
 # Get data files. Parse datetimes and set as index.
 input_data = pd.read_csv(file_paths['paired_station_wl_data'], parse_dates=True, index_col=0)
-target_wl_data = pd.read_csv(file_paths['target_station_wl_data'], parse_dates=True, index_col=0)
+target_data = pd.read_csv(file_paths['target_station_wl_data'], parse_dates=True, index_col=0)
 
 # Store paired station water level and wind.
 input_wl_data = input_data.drop(columns=[input_data.columns[1:]])
-input_wind_data = input_data.drop(columns=[input_data.columns[1:3]])
+input_wind_data = target_data.loc[input_data.columns[1:3]]
 
-# all_data_temp
-all_data = pd.merge(target_wl_data, input_wl_data, left_index=True, right_index=True, how='left',
+all_data_temp = pd.merge(target_data, input_wl_data, left_index=True, right_index=True, how='left',
                          suffixes=('_target', '_input'))
-# all_data = pd.merge(all_data_temp, input_wind_data, left_index=True, right_index=True, how='left')
 
 # Store column names for slicing later.
-# input_wind_cols = input_wind_data.columns.tolist()
 input_wl_col = input_wl_data.columns[0]
-target_col = target_wl_data.columns[0]
+input_wind_cols = input_wind_data.columns.tolist()
+target_col = target_data.columns[0]
 
-# Forward fill 6-minute slots with hourly wind. Limit forward filling up to 9 points to only represent the hour.
-# all_data['u'], all_data['v'] = all_data['u'].ffill(limit=9), all_data['v'].ffill(limit=9)
+''' ************************************************ CREATE UV WIND ************************************************ '''
+input_uv_wind_cols = ['u', 'v']
 
+# Convert to radians and flip direction (meteorological convention: direction wind is FROM).
+speed, direction = input_wind_data[input_wind_cols[0]], input_wind_data[input_wind_cols[1]]
+theta = np.radians(270 - direction)  # 270° is to align 0° as TO north.
+
+u = speed * np.cos(theta)
+v = speed * np.sin(theta)
+
+input_wind_data['u'] = u
+input_wind_data['v'] = v
+input_wind_data = input_wind_data[input_uv_wind_cols]  # Drop speed & direction columns, keep the new u & v.
+
+# Make hourly-averaged uv wind dataframe.
+if config['output_settings']['do_hourly_average_on_wind']:
+    input_wind_data = input_wind_data[['u', 'v']].resample('1H').mean()  # Hourly dataframe.
+    # Merging the 6-minute forward filled hourly wind will help us accurately drop NaNs in the next section.
+    all_data = pd.merge(all_data_temp, input_wind_data, left_index=True, right_index=True, how='left')
+    all_data['u'], all_data['v'] = all_data['u'].ffill(limit=9), all_data['v'].ffill(limit=9)
+else:
+    all_data = pd.merge(all_data_temp, input_wind_data, left_index=True, right_index=True, how='left')
+
+''' *************************************** GROUP CONSECUTIVE INPUT VALUES  *************************************** '''
 # All rows where all columns are non-NaN.
 # valid_df = all_data[all_data[all_data.columns].notna().all(axis=1)].copy()
 valid_df = all_data.dropna().copy()
@@ -57,11 +81,14 @@ group_sizes = valid_df.groupby('group').size()
 window_size_past_list = config['output_settings']['window_size_past']
 window_size_future_list = config['output_settings']['window_size_future']
 
-# Store wl and wind window sizes. Convert window size for wind in hours to 6-minute.
+# Store wl and wind window sizes. Convert window size for wind to units of the water levels, necessary for
+# getting correct minimum grouping size. wind_time_per_wl_time=1 if wind and wl are same unit, =10 if wind is hourly
+# but wl is 6 min, etc.
+wind_time_per_wl_time = input_wind_data.index.to_series().diff()[1] / all_data.index.to_series().diff()[1]
 wl_past_window_size = window_size_past_list[0]
-wind_past_window_size = window_size_past_list[1] * 10
+wind_past_window_size = window_size_past_list[1] * wind_time_per_wl_time
 wl_future_window_size = window_size_past_list[0]
-wind_future_window_size = window_size_past_list[1] * 10
+wind_future_window_size = window_size_past_list[1] * wind_time_per_wl_time
 
 max_past_window_size = max(wl_past_window_size, wind_past_window_size)
 max_future_window_size = max(wl_future_window_size, wind_future_window_size)
@@ -72,6 +99,7 @@ min_required_group_size = max_past_window_size + max_future_window_size + 1
 # Get groups long enough to map inputs to a target sample.
 valid_groups = group_sizes[group_sizes >= min_required_group_size]
 
+''' ******************************************* BUILD MLP INPUT ARRAYS ******************************************** '''
 # Iterate over all segments, getting inputs (past and future 6-min water levels + past hourly wind) in slices to 
 # store in array.
 inputs_arr = []
@@ -84,24 +112,24 @@ for group_id in valid_groups.index:
         break
 
     group_df = valid_df[valid_df['group'] == group_id]
-    # group_df = group_df.sort_index()  # Ensure chronological order.
 
     # Convert columns to numpy for faster slicing.
     wl_values = group_df[input_wl_col].to_numpy()
-    # wind_values = group_df[input_wind_cols].to_numpy()
+    wind_values = group_df[input_uv_wind_cols].to_numpy()
     targets = group_df[target_col].to_numpy()
     timestamps = group_df.index.to_numpy()
 
     # Get all samples in the segment that are bounded by full past/future windows.
-    # Extract wind values one hour (10 points) apart.
     for i in range(max_past_window_size, len(group_df) - max_future_window_size):
         wl_past = wl_values[i - wl_past_window_size: i]
         wl_future = wl_values[i + 1: i + 1 + wl_future_window_size]
-        # wind_past = wind_values[i - wind_past_window_size: i: 10]
-        # wind_future = wind_values[i + 1: i + 1 + wind_future_window_size: 10]
+
+        # Extract wind values in steps scaled by the water level spacing.
+        wind_past = wind_values[i - wind_past_window_size: i: wind_time_per_wl_time]
+        wind_future = wind_values[i + 1: i + 1 + wind_future_window_size: wind_time_per_wl_time]
 
         # Flatten wind (2D) and concatenate everything into a 1D input.
-        input_seq = np.concatenate([wl_past, wl_future])#, wind_past.flatten()])
+        input_seq = np.concatenate([wl_past, wl_future, wind_past.flatten()])
 
         target_val = targets[i]
         target_time = timestamps[i]
